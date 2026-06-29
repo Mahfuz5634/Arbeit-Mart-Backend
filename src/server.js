44e7518fa0649b78c48d8c0c8e1bec6881067fc7
Mahfuz <mahfuzru5634@gmail.com>
2026-06-29 14:42:27 +0600

    do setup for vecel host

--- src/server.js
+++ src/server.js
@@ -2,17 +2,16 @@ import app from "./app.js";
 import config from "./config/index.js";
 import connectDb from "./db/index.js";
 
+const port = config.port;
 
-const port=config.port;
 
-async function main(){
-    try {
-        await connectDb();
-       app.listen(port,()=>{
-         console.log(`server is running on port ${port}`);
-       })
-    } catch (error) {
-        console.log(error);
-    }
-}
-main();
+connectDb()
+  .then(() => console.log("Database connected successfully"))
+  .catch((err) => console.log(err));
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+export default app;

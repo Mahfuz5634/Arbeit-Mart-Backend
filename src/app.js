ba919771086980007b547e4261f7762212964986
Mahfuz <mahfuzru5634@gmail.com>
2026-06-29 15:01:20 +0600

    update cors issue

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ import cors from 'cors';
 const app=express();
 app.use(express.json());
 app.use(cors({
-    origin: ["http://localhost:5173","https://arbeit-mart.vercel.app/","http://localhost:5174"],
+    origin: ["http://localhost:5173","https://arbeit-mart.vercel.app","http://localhost:5174"],
     credentials: true
 }));
 
